cff87f9122ce16075ae407316f80b592b5501f12
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2022-04-04 20:12:54 +0300

    Styling modal

--- src/UniversityDetailsModal/UniversityDetailsModal.jsx
+++ src/UniversityDetailsModal/UniversityDetailsModal.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactModal from 'react-modal';
-import { buildFlagImageUrl } from '../utils/flag-utils';
-import './UniversityDetailsModal.css';
-import { CloseIcon } from './CloseIcon';
+import {buildFlagImageUrl} from '../utils/flag-utils';
+import './UniversityDetailsModal.scss';
+import {CloseIcon} from './CloseIcon';
 
 export const UniversityDetailsModal = ({ university, onClose }) => {
   const universityName = university?.['Educational institution'];
@@ -17,27 +17,29 @@ export const UniversityDetailsModal = ({ university, onClose }) => {
       onRequestClose={onClose}
       preventScroll={true}
       className="modal"
-      style={{ overlay: { display: 'flex', justifyContent: 'center', zIndex: 20 } }}
+      style={{overlay: {display: 'flex', justifyContent: 'center', zIndex: 20}}}
     >
-        <div className="modal-header">
-          <div className="modal-title">
-            <div className="modal-title-text">{universityName}</div>
-            <div className="flag mini" style={{ backgroundImage: buildFlagImageUrl(countryCode) }}></div>
-          </div>
-          <button onClick={onClose} className="modal-close-button">
-            <CloseIcon />
-          </button>
+      <button onClick={onClose} className="modal-close-button">
+        <CloseIcon/>
+      </button>
+
+      <div className="modal-header">
+        <div className="modal-title">
+          <div className="modal-title-text">{universityName}</div>
+          <div className="flag mini"
+               style={{backgroundImage: buildFlagImageUrl(countryCode)}}/>
         </div>
+      </div>
 
-        <div className="modal-content">
-          <p><b>Degrees: </b>{degrees}</p>
+      <div className="modal-content">
+        <p className="modal-degree"><b>Degrees: </b>{degrees}</p>
 
-          <p><b>Statement: </b>{statement}</p>
+        {Boolean(statement) && <p><b>Statement: </b>{statement}</p>}
 
           {
             link ? (
               <p>
-                <a href={link} target="_blank">Link To Instructions</a>
+                <a href={link} target="_blank" rel="noreferrer">Link To Instructions</a>
               </p>
             ) : ''
           }
